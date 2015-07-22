d593c82c0e91598aade38519a7678c548c70e6e0
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-22 00:24:27 -0700

    seek_temps spawns a process that coordinates the for each city

--- lib/weather_search.ex
+++ lib/weather_search.ex
@@ -1,2 +1,12 @@
 defmodule WeatherSearch do
+  def seek_temps(cities) do 
+    # 
+    coordinator_pid = spawn(WeatherSearch.Coordinator, :loop, [[], Enum.count(cities)])
+    
+    cities |> Enum.each(fn(city) ->
+      worker_pid = spawn(WeatherSearch.Worker, :loop, [])
+      send( worker_pid, {coordinator_pid, city})
+    end)
+    
+  end
 end
