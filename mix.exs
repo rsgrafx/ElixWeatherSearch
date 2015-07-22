07026692aaeb2a08ac5f01deada74da40d7aedb7
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-22 00:22:56 -0700

    Add dependencies to mix.exs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule WeatherSearch.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule WeatherSearch.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:poolboy, "~> 1.4"},
+      {:httpoison, "~> 0.7"},
+      {:hackney, "~> 1.0"},
+      {:json, "~> 0.3"}]
   end
 end
